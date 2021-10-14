9bfe78719c0c0e2092fdfb004098e4d3d7cc6c09
pdumaran <75081496+pdumaran@users.noreply.github.com>
2021-10-14 12:37:29 +0800

    update

--- jumper/game/words.py
+++ jumper/game/words.py
@@ -1,6 +1,9 @@
+import random
+
 class Words:
     def __init__(self):
         self.user_letter = ['_']
+        self.secret_word = self.random_word()
 
     def can_guess(self, user_input):
         if user_input not in self.user_letter:
@@ -8,3 +11,10 @@ class Words:
             return True
         else:
             return False
+    
+    def verify_letter(self, user_input):
+        return user_input in self.secret_word
+    
+    def random_word(self):
+        self.secret_word = random.choice(self.words)
+        return self.secret_word
