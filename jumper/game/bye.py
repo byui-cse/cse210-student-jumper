8fec2221819afed4ba65b63c3dea429bdf06dea0
RealThadeous <bor21004@byui.edu>
2021-10-14 22:07:55 +0200

    Grant updates for Director, Welcome, and Bye Classes

---
+++ jumper/game/bye.py
@@ -0,0 +1,14 @@
+class Bye():
+    """Displays a goodbye message to the player at the end of the game.
+
+    Stereotype:
+        Information Holder
+    """
+    def __init__(self) -> None:
+        """The class constructor.
+          
+          Args:
+              self (Bye): an instance of Bye.
+        """
+        self.message = """Thank you for playing Jumper, version Team 2. 
+We hope you enjoyed it and we hope to see you again soon."""
