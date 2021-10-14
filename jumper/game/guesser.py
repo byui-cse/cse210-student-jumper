c454b811b9d36d3b4c8b980b6d2e3e7687fe8f13
pdumaran <75081496+pdumaran@users.noreply.github.com>
2021-10-15 06:03:44 +0800

    Create guesser.py

---
+++ jumper/game/guesser.py
@@ -0,0 +1,9 @@
+class Guesser:
+  def get_user_guess(self, prompt):
+        while True:
+            user_letter = input(prompt)
+            
+            if user_letter.isalpha() and len(user_letter) == 1:
+                return user_letter.lower()
+            
+            print("Please enter a single letter only.")
