ec1a22f5ef3901e25bd2bd17d52ca88ba4391703
pdumaran <75081496+pdumaran@users.noreply.github.com>
2021-10-14 20:40:42 +0800

    update

--- jumper/game/console.py
+++ jumper/game/console.py
@@ -1,11 +1,10 @@
 class Console:
-    def get_letter(self, prompt):
-        go_through = False
-        while not go_through:
-            user_input = input(prompt)
-            if user_input.isalpha() and len(user_input) == 1:
-                return user_input.lower()
-            print("Please enter a single letter.")
-    
-    def write(self,message):
-        print(message)
+
+    def get_user_guess(self, prompt):
+        while True:
+            user_letter = input(prompt)
+            
+            if user_letter.isalpha() and len(user_letter) == 1:
+                return user_letter.lower()
+            
+            print("Please enter a single letter only.")
--- jumper/game/parachute.py
+++ jumper/game/parachute.py
@@ -1,1 +1,33 @@
 class Parachute:
+
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Parachute): an instance of Parachute.
+        """
+        self.num_wrong_guess = 0
+        self.parachute_graphics = ["  ___",
+                                   " /___\\",
+                                   " \\   /",
+                                   "  \\ /",
+                                   "   0",
+                                   "  /|\\",
+                                   "  / \\",
+                                   "\n",
+                                   "^^^^^^^"]        
+    
+    def parachuter(self):
+        if self.num_wrong_guess is not 4:
+            skip = "\n".join(self.parachute_graphics[self.num_wrong_guess:])
+            return skip
+        
+        end = "   X\n" + "\n".join(self.parachute_graphics[self.num_wrong_guess + 1:])
+        return end
+    
+    def guessed_wrong(self):
+        self.num_wrong_guess = self.num_wrong_guess + 1
+    
+    def end(self):
+        max_guess = self.num_wrong_guess >= 4
+        return max_guess
