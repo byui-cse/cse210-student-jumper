dacdfeb395d656d110d4fc1877e00b844cfa5b58
soldochris <soldevilla.christian@gmail.com>
2021-10-14 15:59:42 -0500

    console class done

--- jumper/game/console.py
+++ jumper/game/console.py
@@ -7,4 +7,20 @@ class Console:
     Attributes:
         prompt (string): The prompt to display on each line.
     """
-    
+    def read(self, prompt):
+        """Gets text input from the user through the screen.
+        Args: 
+            self (Screen): An instance of Screen.
+            prompt (string): The prompt to display to the user.
+        Returns:
+            string: The user's input as text.
+        """
+        return input(prompt)
+        
+    def write(self, text):
+        """Displays the given text on the screen. 
+        Args: 
+            self (Screen): An instance of Screen.
+            text (string): The text to display.
+        """
+        print(text)
