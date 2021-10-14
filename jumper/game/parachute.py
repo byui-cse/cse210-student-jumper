e920678995083f5e6a847ccb01d6b08fb892dbac
pdumaran <75081496+pdumaran@users.noreply.github.com>
2021-10-15 06:04:07 +0800

    Update parachute.py

--- jumper/game/parachute.py
+++ jumper/game/parachute.py
@@ -5,8 +5,7 @@ class Parachute:
         
         Args:
             self (Parachute): an instance of Parachute.
-        """
-        self.num_wrong_guess = 0
+        """        
         self.parachute_graphics = ["  ___",
                                    " /___\\",
                                    " \\   /",
@@ -15,8 +14,9 @@ class Parachute:
                                    "  /|\\",
                                    "  / \\",
                                    "\n",
-                                   "^^^^^^^"]        
-    
+                                   "^^^^^^^"]
+        self.num_wrong_guess = 0
+        
     def parachuter(self):
         if self.num_wrong_guess is not 4:
             skip = "\n".join(self.parachute_graphics[self.num_wrong_guess:])
@@ -25,9 +25,9 @@ class Parachute:
         end = "   X\n" + "\n".join(self.parachute_graphics[self.num_wrong_guess + 1:])
         return end
     
-    def guessed_wrong(self):
-        self.num_wrong_guess = self.num_wrong_guess + 1
-    
     def end(self):
         max_guess = self.num_wrong_guess >= 4
         return max_guess
+    
+    def guessed_wrong(self):
+        self.num_wrong_guess = self.num_wrong_guess + 1
