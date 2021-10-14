13c8acdfb3a600cd5d326dd1d75129eaf2a71739
pdumaran <75081496+pdumaran@users.noreply.github.com>
2021-10-15 05:30:18 +0800

    Update word.py

--- jumper/game/word.py
+++ jumper/game/word.py
@@ -39,3 +39,12 @@ class Word:
             return False
         else:
             return True
+        
+    def get_user_guess(self, prompt):
+        while True:
+            user_letter = input(prompt)
+            
+            if user_letter.isalpha() and len(user_letter) == 1:
+                return user_letter.lower()
+            
+            print("Please enter a single letter only.")
