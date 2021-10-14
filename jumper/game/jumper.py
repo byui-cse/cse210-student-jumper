c17d12783139bf44913869f037cdcd2b93c638e7
Marianne Johnson <joh20041@byui.edu>
2021-10-13 20:06:07 -0700

    finished get_current_word()

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -2,4 +2,29 @@
 
 class Jumper:
 
-  pass
+  def get_current_word():
+    """Keeps track of which letters are currently hidden and revealed.
+
+       Args:
+          self (Puzzle): An instance of Puzzle.
+       Returns:
+          List
+    """
+    current_word = []
+    still_missing = True
+
+    for word_letter in word:
+      for correct_letter in correct_letters:
+
+        if correct_letter.lower() == word_letter:
+          current_word.append(word_letter)
+          still_missing = False
+          break
+
+        else:
+          still_missing = True
+      
+      if still_missing:
+        current_word.append('_')
+    
+    return current_word
