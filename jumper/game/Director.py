436b6cbc4b33c510504c884c5139229027132ac6
herrmeyerL <logern5@gmail.com>
2021-10-14 14:35:24 -0600

    Create director class

---
+++ jumper/game/Director.py
@@ -0,0 +1,13 @@
+class Director:
+    """
+    Code for the director of the game
+    """
+    def __init__(self):
+        """
+        Class constructor
+        """
+        pass
+    def get_inputs(self):
+        pass
+    def do_outputs(self):
+        pass
