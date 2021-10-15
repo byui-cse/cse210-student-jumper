5aa8d23bc6163e0446964966e56c68c61fa05be5
Jeremy Jackson <jectproxisdev@gmail.com>
2021-10-15 11:24:11 -0600

    Added comments to the Jumper class.

--- jumpergame/game/jumper.py
+++ jumpergame/game/jumper.py
@@ -4,6 +4,13 @@ from game.wordProcessor import WordProcessor
 class Jumper:
     """A class dedicated to printing the jumper and keeping track of points."""
     def __init__(self):
+        """The class constructor.
+
+        keepPlaying: Bool to keep track of whether the user has lost
+        wrongGuess: counter to keep track of wrong guesses
+        processor: an instance of the word processor class
+        asciiart: a list to build the jumper art
+        """
         self.keepPlaying = True
         self.wrongGuess = 0
         self.processor = WordProcessor()
@@ -19,15 +26,32 @@ class Jumper:
         
 
     def printJumper(self):
+        """Method for printing the jumper in ASCII art.
+        
+        Args:
+            self(Jumper): an instance of the jumper class
+        """
         for line in self.asciiArt:
             print(line)
 
     def checkCorrect(self, letter):
+        """Method for checking if the user's guess is correct.
+        
+        Args:
+            self(Jumper): an instance of the Jumper class.
+            letter: the letter guessed by the user.
+        """
         if (not self.processor.checkLetter(letter)):
             self.wrongGuess += 1
             self.asciiArt.remove(0)
     
     def continuePlay(self, letter):
+        """Method for checking if the game is gameover.
+        
+        Args:
+            self(Jumper): Object of the Jumper class.
+            letter: Guess from the user.
+        """
         if letter == '  0  ':
             self.asciiArt[0] = '  X  '
             self.keepPlaying = False
