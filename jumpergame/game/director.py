4ce30dc36e64fdf8f2491df1cc41f0fc2f55a87b
Jeremy Jackson <jectproxisdev@gmail.com>
2021-10-15 12:05:26 -0600

    adding notes to the director class

--- jumpergame/game/director.py
+++ jumpergame/game/director.py
@@ -3,12 +3,24 @@ from game.wordList import WordList
 from game.wordProcessor import WordProcessor
 
 class Director:
+    """Director class to direct all the aspects of the game."""
     
     def __init__(self):
+        """Class constructor.
+
+        jumper: An instance of the jumper class
+        wordlist: an instance of the wordlist class
+        wordProcessor: an instance wordprocessor class.
+        """
         self.jumper = Jumper()
         self.wordList = WordList('words.txt')
         self.wordProcessor = WordProcessor()
 
     def startGame(self):
+        """A method to display the starting screen of the game.
+        
+        Args: 
+            self(Director): An instance of the Director class.
+        """
         self.wordProcessor.printWord()
         self.jumper.asciiArt()
