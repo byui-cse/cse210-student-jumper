3225e0208b5a1258e49e25431a4b36d543ee7dc2
wiseman2star <wiseman2star@gmail.com>
2021-10-14 22:34:39 -0600

    finished part, added most comments

--- jumper/game/guesser.py
+++ jumper/game/guesser.py
@@ -1,6 +1,16 @@
 class Guesser:
     """make guess"""
+    """A code template for the guesser who tries to figure out the word. 
+    The responsibility of this class of objects is to guess the word before
+    the jumper loses all the strings of their parachute.
     
+    Stereotype:
+        Information Holder
+
+    Attributes:
+        Useless, . . . maybe.
+    """
+
     def read_response():
         """hold question text
         string
