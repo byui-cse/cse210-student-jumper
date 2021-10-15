4c23abeabea70c12fb458a1283814530c684e1d2
kyeguy00 <91562786+kyeguy00@users.noreply.github.com>
2021-10-14 19:39:32 -0600

    Updated it to return the word

--- jumper/game/get_word.py
+++ jumper/game/get_word.py
@@ -3,16 +3,16 @@ import glob
 import os
 
 class SecretWord:
-    os.chdir(r'cse210-student-jumper')
-    myFiles = glob.glob('*.txt')
-    os.chdir(r'jumper')
-    myFiles2 = glob.glob('*.txt')
-    os.chdir(r'game')
-    myFiles3 = glob.glob('*.txt')
+    def get_word():
+        os.chdir(r'cse210-student-jumper')
+        myFiles = glob.glob('*.txt')
+        os.chdir(r'jumper')
+        myFiles2 = glob.glob('*.txt')
+        os.chdir(r'game')
+        myFiles3 = glob.glob('*.txt')
 
-    index = random.randint(0,844)
-    file = open('words.txt', 'r')
-    l = file.readlines()
-    selected_word = l[index].strip('\n')
-
-    print(selected_word)
+        index = random.randint(0,844)
+        file = open('words.txt', 'r')
+        l = file.readlines()
+        selected_word = l[index].strip('\n')
+        return selected_word
