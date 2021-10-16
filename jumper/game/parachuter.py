a038f7bacd477948258825a62d2d9dfc3185efe0
joshudo <joshua.d.ortiz93@gmail.com>
2021-10-15 19:30:40 -0600

    adding comments to parachute.py

--- jumper/game/parachuter.py
+++ jumper/game/parachuter.py
@@ -1,6 +1,20 @@
 class parachuter:
+    """Code to represent the jumper image.
 
+    Stereotype:
+        Information holder
+
+    Attributes:
+        the_parachuter(list): hold the list with the string objects for the jumper image
+        tricts(integer): counter to be able to print 3 strings at a time
+        tries(integer): counter for the number of tries 
+    """
     def __init__(self):
+        """Class constructor, Declares and initializes instance attributes.
+        
+        Args:
+            self (Parachuter): An instance of Parachuter
+        """
         self.the_parachuter = [
             ' ','___',' ',
             '/','___','\\',
@@ -17,12 +31,29 @@ class parachuter:
         
     
     def print_parachuter(self):
+        """Prints the image of the jumper
+
+        Args:
+            self (Parachuter): An isntance of Parachuter
         
+        Returns:
+            list that is printed in 3x5 order
+        """
         for i in self.the_image:
             print(f'{self.the_image[self.ticks]} {self.the_image[self.ticks+1]} {self.the_image[self.ticks+2]}')
             self.ticks += 3
 
     def update_parachuter(self):
+        """Keeps track of the number of tries the player has failed 
+        and updates the list to cut the parachute.
+
+        Args:
+            self (Parachuter): An isntance of Parachuter
+
+        Returns:
+            an instance of self.print_parachute to the director
+        """
+    
         self.tries += 1
 
         if self.tries == 1:
@@ -38,4 +69,4 @@ class parachuter:
             self.the_parachuter[10] = '  '
             self.the_parachuter[13] = 'x'
 
-        return self.the_parachuter
+        return self.print_parachuter
