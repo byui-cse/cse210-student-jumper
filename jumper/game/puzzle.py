5756e5106038d00400443107c80ee584eba846d7
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-10-16 00:02:38 -0600

    Initial file read

--- jumper/game/puzzle.py
+++ jumper/game/puzzle.py
@@ -2,15 +2,22 @@ import random
 
 class Puzzle:
 
-
-    def __init__(self):
-
-        words = ["random", "horse", "computer", "recreation", "scriptures", "family", "gratitude", "creation", "unity", "fantastic", "growth", "majesty", "mountain", "ocean", "basket"]
-        self.word = random.choice(words)
+    def __init__(self, wordlist_filename='../data/mit-wordlist.txt'):
+        with open(wordlist_filename, 'r') as wordlist_file:
+            self.wordlist_size = len([0 for _ in wordlist_file])
+            wordlist_file.seek(random.randint(0, self.wordlist_size - 1))
+            self.chosen_word = wordlist_file.readline(1)
 
     def get_word_line(self):
         pass
 
     def get_jumper(self):
-        jumper = [" _____","/_____\\","\\     /"," \\   /", "   0", "  /|\\", "  / \\"]
+        jumper = (" _____",
+                  "/_____\\",
+                  "\\     /",
+                  " \\   /",
+                  "   0",
+                  "  /|\\",
+                  "  / \\")
         return jumper
+
